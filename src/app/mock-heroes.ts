7a95619dfa8407e45e106c2bf06044960811a96f
Gio Celis <Gio@MacBook-Air.local>
2017-11-13 22:15:40 -0800

    My site ready for deployment.

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Superman' },
+  { id: 2, name: 'Batman' },
+  { id: 3, name: 'Wonder Woman' },
+  { id: 4, name: 'Spider Man' },
+  { id: 5, name: 'Iron Man' },
+  { id: 6, name: 'Aquaman' },
+  { id: 7, name: 'Cyborg' },
+  { id: 8, name: 'Flash' },
+ 
+];
